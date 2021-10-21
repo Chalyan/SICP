31acdf2dad03a734a87d1a1468cb26a220e90f5b
Phaze <gorchalyan@gmail.com>
2021-10-21 19:10:06 +0400

    [REFACTOR] Fixed formating

--- Building_abstractions_with_procedures/The_elements_of_programming/Square_Roots_by_Newtons_Method/NewtonSecondMethod.scm
+++ Building_abstractions_with_procedures/The_elements_of_programming/Square_Roots_by_Newtons_Method/NewtonSecondMethod.scm
@@ -1,24 +1,26 @@
 (define (square x) (* x x))
+
 (define (average x y) (/ (+ x y) 2))
+
 (define (abs x)
     (if (< x 0)
         (- x)
         x))
+
 (define (improve guess x)
-(average guess (/ x guess)))
+    (average guess (/ x guess)))
+
 (define (good_enough? guess x)
-    (< 
-        (/ 
-            (abs 
-                (- guess 
-                (improve guess x)
-                )
-            ) 
+    (< (/ 
+        (abs (- guess (improve guess x))) 
         guess)
     0.00000031415926535)
     )
+
 (define (sqrt_iter guess x)
-(if (good_enough? guess x) guess 
-(sqrt_iter (improve guess x) x)))
+    (if (good_enough? guess x) guess 
+        (sqrt_iter (improve guess x) x)))
+
 (define (sqrt x) (if (= x 0) 0 (sqrt_iter 1.0 x)))
+
 (sqrt 9)
