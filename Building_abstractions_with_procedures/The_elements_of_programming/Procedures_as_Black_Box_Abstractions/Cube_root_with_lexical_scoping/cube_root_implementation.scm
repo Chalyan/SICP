5e705da05de34d65314c6b4242e9129103c02f27
Phaze <gorchalyan@gmail.com>
2021-11-13 16:55:45 +0400

    [fix] Fixed folder names and added unit-tests for square and cube functions

---
+++ Building_abstractions_with_procedures/The_elements_of_programming/Procedures_as_Black_Box_Abstractions/Cube_root_with_lexical_scoping/cube_root_implementation.scm
@@ -0,0 +1,20 @@
+(include "function_module.scm")
+
+(import functions)
+
+(define (cube_root number)
+
+    (define (good-enough? parameter)
+        (< (abs (/ (abs (- (cube parameter) number)) number)) 0.00000000314159))
+
+    (define (better_guess x)
+        (/ (+ (/ number (square x)) (* 2 x)) 3))
+
+    (define (cube_root_iter initial_guess)
+        (if (good-enough? initial_guess)
+            initial_guess
+            (cube_root_iter (better_guess initial_guess))))
+
+    (cond ((= number 0) 0)
+        ((< number 0) (cube_root_iter (- 1.0)))
+        ((> number 0) (cube_root_iter 1.0))))
