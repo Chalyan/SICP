aa6a8393b7b7ee94065f52be51c32d2e0acea27d
phaze <gor.chalyan@qt-lab.com>
2022-01-25 01:24:47 -0800

    [Refactor]
    Rechecked all the files, fixed formatting

--- Building_abstractions_with_procedures/Procedures_and_the_processes_they_generate/Exponentiation/Iterative_successive_squaring/Sources/iterative_exponentiation.scm
+++ Building_abstractions_with_procedures/Procedures_and_the_processes_they_generate/Exponentiation/Iterative_successive_squaring/Sources/iterative_exponentiation.scm
@@ -1,6 +1,7 @@
 (include "../../../../The_elements_of_programming/Procedures_as_Black_Box_Abstractions/Cube_root_with_lexical_scoping/Square_function/Sources/square.scm")
 
 (module iterative-exponentiation (fast-expt)
+    
     (import scheme)
     
     (import square_func)    
