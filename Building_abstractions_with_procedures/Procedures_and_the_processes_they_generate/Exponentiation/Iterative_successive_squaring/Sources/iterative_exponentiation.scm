50e8c9fd8a9b067b0666b8eb43989b503c994b95
Phaze <gorchalyan@gmail.com>
2022-01-27 16:18:20 +0400

    [Refactor]
    Changed implementation to more structured form, so that the business
    logic stayed the same.

--- Building_abstractions_with_procedures/Procedures_and_the_processes_they_generate/Exponentiation/Iterative_successive_squaring/Sources/iterative_exponentiation.scm
+++ Building_abstractions_with_procedures/Procedures_and_the_processes_they_generate/Exponentiation/Iterative_successive_squaring/Sources/iterative_exponentiation.scm
@@ -10,15 +10,13 @@
         (fast-expt-iter number exponent 1))
 
     (define (fast-expt-iter number effective-exponent accumulator)
-        (cond ((and (= effective-exponent 0) (= number 0)) (- 1))
+        (if (= effective-exponent 0)
+            (if (= number 0)
+                (- 1)
+                accumulator)
             
-            ((and (= effective-exponent 0) (not (= number 0))) accumulator)
-            
-            ((and (= number 0) (not (= effective-exponent 0))) 0)
-            
-            ((and (= number 1) (not (= effective-exponent 0))) 1)
-            
-            ((and (not (= number 0)) (not (= effective-exponent 0))) 
+            (if (or (= number 0) (= number 1))
+                number
                 (if (= (remainder effective-exponent 2) 0)
                     (fast-expt-iter (square number) (/ effective-exponent 2) accumulator)
                     (fast-expt-iter number (- effective-exponent 1) (* accumulator number)))))))
